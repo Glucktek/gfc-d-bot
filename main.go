0476614f164fe6c110fc17470080c8a5e57a6ce6
agluck91 <andrew.gluck91@gmail.com>
2025-05-04 16:15:36 -0400

    Bot initial code

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/glucktek/gfc-d-bot/pkgs/lightsail"
+)
+
+func main() {
+
+	fmt.Println("Starting bot service...")
+
+	// Initialize the Lightsail client
+	client, err := lightsail.NewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	// HACK: Using hardcaded instance name for now
+	instanceName := "GreaterFaithChurchSite"
+
+	// Get status
+	state, err := client.GetInstanceState(ctx, instanceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Instance state: %s\n", state)
+
+	// Start instance
+	// if err := client.StartInstance(ctx, instanceName); err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// // Stop instance
+	// if err := client.StopInstance(ctx, instanceName); err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// // Reboot instance
+	// if err := client.RebootInstance(ctx, instanceName); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+}
---
+++ pkgs/lightsail/lightsail_functions.go
@@ -0,0 +1,55 @@
+package lightsail
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/lightsail"
+)
+
+type Client struct {
+	ls *lightsail.Client
+}
+
+func NewClient() (*Client, error) {
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		ls: lightsail.NewFromConfig(cfg),
+	}, nil
+}
+
+func (c *Client) StartInstance(ctx context.Context, instanceName string) error {
+	_, err := c.ls.StartInstance(ctx, &lightsail.StartInstanceInput{
+		InstanceName: &instanceName,
+	})
+	return err
+}
+
+func (c *Client) StopInstance(ctx context.Context, instanceName string) error {
+	_, err := c.ls.StopInstance(ctx, &lightsail.StopInstanceInput{
+		InstanceName: &instanceName,
+		Force:        false,
+	})
+	return err
+}
+
+func (c *Client) RebootInstance(ctx context.Context, instanceName string) error {
+	_, err := c.ls.RebootInstance(ctx, &lightsail.RebootInstanceInput{
+		InstanceName: &instanceName,
+	})
+	return err
+}
+
+func (c *Client) GetInstanceState(ctx context.Context, instanceName string) (string, error) {
+	resp, err := c.ls.GetInstance(ctx, &lightsail.GetInstanceInput{
+		InstanceName: &instanceName,
+	})
+	if err != nil {
+		return "", err
+	}
+	return string(*resp.Instance.State.Name), nil
+}
